e8095d0770f42e2d48850f9047b91b5cf6d2381a
Lucas Spagnoli <lm.spanholi@gmail.com>
2026-08-19 15:08:31 -0300

    ex3: create dados functions

--- atividade3/src/dados/Cliente.java
+++ atividade3/src/dados/Cliente.java
@@ -1,4 +1,58 @@
 package dados;
 
 public class Cliente {
+    private int cpf;
+    private String nomme;
+    private String endereco;
+    private int telefone;
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNomme() {
+        return nomme;
+    }
+
+    public void setNomme(String nomme) {
+        this.nomme = nomme;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "cpf=" + cpf +
+                ", nomme='" + nomme + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", telefone=" + telefone +
+                '}';
+    }
+
+    public void reservarIda(Reserva reserva) {
+
+    }
+
+    public void reservarVolta(Reserva ida, Reserva volta) {
+        
+    }
 }
--- atividade3/src/dados/Reserva.java
+++ atividade3/src/dados/Reserva.java
@@ -1,4 +1,80 @@
 package dados;
 
 public class Reserva {
+    private int numReserva;
+    private String dataVoo;
+    private String horaVoo;
+    private float preco;
+    private String classeVoo;
+    private boolean idaEvolta;
+    private int poltrona;
+
+    @Override
+    public String toString() {
+        return "Reserva{" +
+                "numReserva=" + numReserva +
+                ", dataVoo='" + dataVoo + '\'' +
+                ", horaVoo='" + horaVoo + '\'' +
+                ", preco=" + preco +
+                ", classeVoo='" + classeVoo + '\'' +
+                ", idaEvolta=" + idaEvolta +
+                ", poltrona=" + poltrona +
+                '}';
+    }
+
+    public int getNumReserva() {
+        return numReserva;
+    }
+
+    public void setNumReserva(int numReserva) {
+        this.numReserva = numReserva;
+    }
+
+    public String getDataVoo() {
+        return dataVoo;
+    }
+
+    public void setDataVoo(String dataVoo) {
+        this.dataVoo = dataVoo;
+    }
+
+    public String getHoraVoo() {
+        return horaVoo;
+    }
+
+    public void setHoraVoo(String horaVoo) {
+        this.horaVoo = horaVoo;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public String getClasseVoo() {
+        return classeVoo;
+    }
+
+    public void setClasseVoo(String classeVoo) {
+        this.classeVoo = classeVoo;
+    }
+
+    public boolean isIdaEvolta() {
+        return idaEvolta;
+    }
+
+    public void setIdaEvolta(boolean idaEvolta) {
+        this.idaEvolta = idaEvolta;
+    }
+
+    public int getPoltrona() {
+        return poltrona;
+    }
+
+    public void setPoltrona(int poltrona) {
+        this.poltrona = poltrona;
+    }
 }
