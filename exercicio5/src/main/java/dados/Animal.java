546d9c30126954f148d32fdfdf631d9b8eaa86b5
Lucas Spagnoli <lm.spanholi@gmail.com>
2026-09-14 16:13:25 -0300

    create exercise 5 classes

---
+++ exercicio5/src/main/java/dados/Animal.java
@@ -0,0 +1,8 @@
+package dados;
+
+public class Animal {
+    private String nome;
+    private String cor;
+    private String especie;
+    private int idade;
+}
---
+++ exercicio5/src/main/java/dados/Viveiro.java
@@ -0,0 +1,23 @@
+package dados;
+
+public class Viveiro {
+    private String nome;
+    private float comprimento;
+    private float largura;
+
+    public float calculaEspaco(){
+
+    }
+
+    private float espacoOcupado(){
+
+    }
+
+    private float espacoDisponivel(){
+
+    }
+
+    public boolean adicionarAnimal(Animal animal){
+
+    }
+}
